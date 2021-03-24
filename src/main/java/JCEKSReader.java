a9207297af43a610aa6e3587b42ad3f3fe1f8f9e
Prasanth Jayachandran <pjayachandran@cloudera.com>
2021-03-24 18:07:01 +0530

    read password as 2nd argument

--- src/main/java/JCEKSReader.java
+++ src/main/java/JCEKSReader.java
@@ -17,8 +17,11 @@ public class JCEKSReader {
   // kubectl cp warehouse-1572892161-gfh4/metastore-0:/jceks/..data/secrets.jceks secrets.jceks
   public static void main(String[] args)
     throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
-    String fileName = args.length == 1 ? args[0] : "/tmp/secrets.jceks";
-    char[] password = "none".toCharArray();
+    String fileName = args.length >= 1 ? args[0] : "/tmp/secrets.jceks";
+    System.out.println("File: " + fileName);
+    String passwd = args.length >= 2 ? args[1] : "none";
+    System.out.println("Password to unlock: " + passwd);
+    char[] password = passwd.toCharArray();
     KeyStore ks = KeyStore.getInstance("JCEKS");
     try (FileInputStream fis = new FileInputStream(fileName)) {
       ks.load(fis, password);
